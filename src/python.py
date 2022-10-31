cbacf2ecf48625ce54a84e27dc53aa17750eca5e
byuengyun Chae <chaeby7@gmail.com>
2022-10-31 16:57:52 +0900

    change route

--- src/python.py
+++ src/python.py
@@ -21,7 +21,7 @@ md = torch.load(
 md.eval()
 
 test_img = Image.open(
-    'C:\\Users\\user\\Documents\\fish\\uploads\\google2001.jpg').convert('RGB')
+    'C:/Users/user/Documents/fish/uploads/google2001.jpg').convert('RGB')
 preprocess = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
