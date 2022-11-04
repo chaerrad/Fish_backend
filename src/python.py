967f8425ee7fab9205b3d75a0cb11abc2eddb951
byuengyun Chae <chaeby7@gmail.com>
2022-11-04 23:27:01 +0900

    Update url

--- src/python.py
+++ src/python.py
@@ -17,11 +17,11 @@ import copy
 from PIL import Image
 import torch.nn.functional as F
 md = torch.load(
-    'C:/Users/user/Documents/fish/src/resnet_total_with_test.pt', map_location='cpu')
+    '/root/fish_backend/src/resnet_total_with_test.pt', map_location='cpu')
 md.eval()
 
 test_img = Image.open(
-    'C:/Users/user/Documents/fish/uploads/google2001.jpg').convert('RGB')
+    '/root/fish_backend/uploads/google2001.jpg').convert('RGB')
 preprocess = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
